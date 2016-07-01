145f4f31c8cda4c942eaa3e0bd0bc322ecb03779
LordMike <lordmike@iki.fi>
2016-07-02 01:18:52 +0300

    added the core code

---
+++ Source.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+using namespace std;
+
+int printToConsole(char* msg)
+{
+	cout << endl << endl << msg << endl << endl;
+
+	return 0;
+}
---
+++ Source.h
@@ -0,0 +1,2 @@
+
+int printToConsole(char* msg);
